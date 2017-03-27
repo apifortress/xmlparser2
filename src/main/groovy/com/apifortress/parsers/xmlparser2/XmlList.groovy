21f2d71a086da93df34b52c2baf7ecb3787c7eab
theirish81 <theirish81@localhost>
2017-03-27 16:35:14 +0200

    original import

---
+++ src/main/groovy/com/apifortress/parsers/xmlparser2/XmlList.groovy
@@ -0,0 +1,35 @@
+package com.apifortress.parsers.xmlparser2
+
+import groovy.util.slurpersupport.GPathResult
+
+/**
+ *
+ */
+class XmlList extends LinkedList<XmlNode> implements IXmlItem {
+
+    public static XmlList convert(GPathResult result){
+        XmlList list = new XmlList()
+        result.each {
+            list.add(new XmlNode(it))
+        }
+        return list;
+    }
+
+    public XmlList(List<XmlNode> item){
+        super()
+        addAll(item)
+    }
+
+    private XmlList(){
+        super()
+    }
+
+    public IXmlItem get(String name){
+        def items = findAll { it.name == name }
+        if(items.isEmpty())
+            return null
+        if(items.size()==1)
+            return items[0]
+        return new XmlList(items)
+    }
+}
