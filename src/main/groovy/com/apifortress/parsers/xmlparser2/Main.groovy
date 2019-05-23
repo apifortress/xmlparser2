df10347aa96acae4e91b56120bc737489e592b1d
theirish81 <theirish81@localhost>
2019-05-23 17:36:41 +0200

    asXML

--- src/main/groovy/com/apifortress/parsers/xmlparser2/Main.groovy
+++ src/main/groovy/com/apifortress/parsers/xmlparser2/Main.groovy
@@ -7,8 +7,6 @@ class Main {
 
     public static void main(def args){
         XmlNode node = new XmlNode(new XmlSlurper().parse(new File('stuff2.xml')))
-        node.Artist.pick(5).each{
-            println it['@href']
-        }
+        println(node.toString())
     }
 }
